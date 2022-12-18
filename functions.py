403ab9957b2ffb130fd9d50b84946b89e0fe07fb
Zarushy09 <zarushy489@gmail.com>
2022-12-18 15:17:34 -0400

    first commit

---
+++ functions.py
@@ -0,0 +1,49 @@
+""" def mult(a,b):
+    return a * b
+
+resut = mult(10,2)
+
+print(resut)
+
+input("como estas? ")
+ """
+
+# Operadores
+
+
+# # Funciones
+# def lol(name, edad):
+#     print("hola " + name  + " tu edad es: " + str(edad))
+
+# lol("Popa", 19)
+
+
+
+import string
+
+
+name = input("Ingresa tu nombre: ")
+edad = input("Ingresa tu edad: ")
+
+if (name == ""):
+    print("Debes ingresar un nombre: ")
+elif (name != string):
+    print("Ingr")
+
+print(f"Hola {name}, tu eedad es {edad}")
+
+
+# Buclues
+
+
+
+
+""" 
+import string
+
+
+int
+string
+bool 
+float
+ """
---
+++ server/main.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.get()
+
+const PORT =  process.env.PORT
+app.listen(PORT, () => console.log(`Sever running in http://localhost:${PORT}`))
